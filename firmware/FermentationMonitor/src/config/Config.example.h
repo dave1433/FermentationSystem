06634dc60750d66548bcdc76846df5b0ebec1013
dave1433 <raulchuchuca951@gmail.com>
2026-05-02 21:08:09 +0200

    Add Config.example.h template

---
+++ firmware/FermentationMonitor/src/config/Config.example.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// WiFi credentials
+#define WIFI_SSID "your_wifi_name"
+#define WIFI_PASSWORD "your_wifi_password"
+
+// MQTT (Flespi)
+#define MQTT_SERVER "mqtt.flespi.io"
+#define MQTT_PORT 1883
+#define MQTT_TOKEN "your_flespi_token"
+
+// Topics
+#define MQTT_TOPIC "your/topic"
